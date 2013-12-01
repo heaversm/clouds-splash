89df32622248fe23109781e0b644500d4095ef23
Mike Heavers <mheavers@gmail.com>
2013-12-01 12:24:30 -0500

    site pulls content from google spreadsheet and renders via handlebars templates. Incorporate underscore to handle array selection. Potentially use moment for conversion of dates

--- js/site.js
+++ js/site.js
@@ -1,46 +1,93 @@
   var site =  (function (window,document, $){
 
+    var siteData; //all our site data from the google spreadsheet will be stored here;
+
     var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AsJLPh0xcMR3dDl6UjZJbHJjTVVXV2xCR1pfR0drS0E&output=html';
 
     var aboutVisible = false;
 
     function init(){
-      addNavListeners();
       loadSiteData();
     }
 
     function loadSiteData(){
+      //TABLETOP: https://github.com/jsoma/tabletop
       Tabletop.init( {
         key: public_spreadsheet_url,
         callback: showInfo
-      })
+      });
     }
 
     function showInfo(data, tabletop) {
       console.log(data,tabletop);
+      siteData = data;
+      addNavLinks(data.content);
     }
 
-    function addNavListeners(){
-      $('#about-link').on('click',function(e){
-        e.preventDefault();
-        console.log('click');
-        if (aboutVisible){
-          $('#about').fadeTo(500,0,function(){
-            aboutVisible = false;
-            $('#about').hide();
-          });
-        } else {
-          $('#about').show();
-          $('#about').fadeTo(500,1,function(){
-            aboutVisible = true;
-          });
-        }
+    function addNavLinks(content){
+      var source   = $("#navlink-template").html();
+      var template = Handlebars.compile(source);
+      var html    = template(content);
+      $('#nav').html(html);
+      addNavListeners();
+    }
+
+    function addOverlayListeners(){
+      $('.overlay-close').on('click',function(){
+        hideOverlay();
       });
     }
 
-  return {
-    init: init
-  }
+    function addNavListeners(){
+      $('.navlink').on('click',function(e){
+        e.preventDefault();
+        $linkElement = $(this);
+        var linkID = $linkElement.data('id');
+        var externalLink = $linkElement.attr('href');
+
+        if (externalLink == undefined){ //open a content window
+
+          var source, pageContent;
+          if (linkID == "screenings"){ //render the template for screenings
+            source = $('#screenings-template').html();
+            pageContent = siteData.screenings;
+          } else { //render the general content template
+            source = $("#content-template").html();
+            pageContent = _.find(siteData.content.elements, function(element){
+              return element.id == linkID;
+            });
+          }
+
+          var template = Handlebars.compile(source);
+
+          var html = template(pageContent);
+          $('#overlay').html(html);
+          showOverlay();
+
+        } else { //open the external link
+          window.open(externalLink,"_blank");
+        }
+
+      });
+    }
+
+    function hideOverlay(){
+      $('#overlay').fadeTo(500,0,function(){
+        $('#overlay').hide();
+      });
+    }
+
+    function showOverlay(){
+      $('#overlay').show();
+      $('#overlay').fadeTo(500,1,function(){
+        addOverlayListeners();
+      });
+
+    }
+
+    return {
+      init: init
+    }
 
 })(window,document,jQuery);
 
